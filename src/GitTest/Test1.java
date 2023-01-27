ee5a776b113f6c20cd8c4708827cba1e1d756acd
woon <woonsta13@gmail.com>
2023-01-27 07:08:52 -0800

    first commit

---
+++ src/GitTest/Test1.java
@@ -0,0 +1,16 @@
+package GitTest;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        String hungry = "yes";
+
+        if (hungry == "yes")
+            System.out.println("eat something");
+        else {
+            System.out.println("do work");
+        }
+
+    }
+}
